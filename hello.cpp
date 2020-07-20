bd8c235a0fb80392c433de003dc5ae142b57d8e7
praneethaem <68540668+praneethaem@users.noreply.github.com>
2020-07-20 18:47:39 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,4 @@
+using namespace std;
 class Point {
 private:
     int x;
@@ -9,3 +10,13 @@ public:
     inline void setX(int x) { this->x = x; }
     inline void setY(int y) { this->y = y; }
 };
+
+int main()
+{
+    Point P;
+    P.setX(1);
+    P.setY(2);
+    cout << P.getX()<<endl;
+    cout << P.getY()<<endl;
+    return 0;
+ }
