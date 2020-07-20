60abebf41049188e85796a89b7513af538817421
praneethaem <68540668+praneethaem@users.noreply.github.com>
2020-07-20 19:01:29 +0530

    Update testparamchecker.cpp

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -9,7 +9,7 @@ TEST(VitalsTest, SPO2) {
     ASSERT_EQ(false, vitalsAreOk(100, 40, 50));
 }
  
-int main(int argc, char **argv) {
+/*int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-}
+}/*
