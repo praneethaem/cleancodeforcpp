1b939d31eae91b774d86b479d7a8c4440de054fe
praneethaem <68540668+praneethaem@users.noreply.github.com>
2020-07-22 15:04:16 +0530

    Update paramchecker.cpp
    Solved build error

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -12,7 +12,7 @@ bool chk_all_params(float param,float lowerlimit,float higherlimit)
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) 
 {
-bool ret = chk_all_params(bpm, 70 , 150)  && chk_all_params(spo2,80 ,100 ) && chk_all_params(resp_Rate,30,60);
+bool ret = chk_all_params(bpm, 70 , 150)  && chk_all_params(spo2,80 ,100 ) && chk_all_params(respRate,30,60);
 return ret;
 }
 
